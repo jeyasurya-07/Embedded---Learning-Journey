5e422ce3262015f47a5b36790e250f977ac5262c
Jeyasurya-07 <jeyasuryamuniyandi@gmail.com>
2025-12-30 12:05:48 +0530

    Fix some minor syntax errors

--- Structure/sensor_array.c
+++ Structure/sensor_array.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <inttypes.h>
 #define MAX 3
+
 typedef struct{
     uint32_t  id;
     uint32_t  value;
 }sensor_t;
+
 sensor_t s[MAX];
 int main ()
 {
     int i;
-printf("\t\tenter the sensor data");
+printf("\n\tEenter the sensor data");
 
 for(i=0;i<MAX;i++)
 {
     sensor_t *ptr=&s[i];
     printf("\nEnter the sensor %d id and value:",i+1);
-    scanf("%u %u",&ptr->id,&ptr->value);
+    if(scanf("%" SCNu32 " %" SCNu32 ,&ptr->id,&ptr->value)!=2)
+{
+    printf("\nInvalid input");
+    return 0;
+}
 }
 
 for(i=0;i<MAX;i++)
 {
     sensor_t *ptr=&s[i];
-    printf("\nsensor %d : %u %u",i+1,ptr->id,ptr->value);
+    printf("\nsensor %d :%" PRIu32 " %" PRIu32,i+1,ptr->id,ptr->value);
 }
 
 
-}
+
+
+    return 0;
+}   
